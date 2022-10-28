5e0e3bf3a2eeb52593b12fceba31dda972d69ef5
Jihyung Park <100411773+Jihyung-PArk@users.noreply.github.com>
2022-10-28 12:47:35 +1000

    Initial commit :

--- src/week6/RainfallAnalyser.java
+++ src/week6/RainfallAnalyser.java
@@ -2,11 +2,13 @@ package week6;
 
 import textio.TextIO;
 
+import java.util.Arrays;
+
 public class RainfallAnalyser {
 
     public static void main(String[] args) {
 
-        System.out.println("Enter a filename: ");
+        System.out.print("Enter a filename: ");
 
         String filename = TextIO.getln();
         System.out.println(filename);
@@ -14,7 +16,12 @@ public class RainfallAnalyser {
         try {
             TextIO.readFile(filename);
             System.out.println("Reading successfully");
-            generateMonthlyStatistics(filename);
+
+            //generate output file
+            String outputFile = generateOutputfile(filename);
+
+            //save monthly total, min and max to output file
+            generateMonthlyStatistics(outputFile);
         }
         catch (Exception e){
             System.out.println("Error : " + e.getMessage());
@@ -22,17 +29,73 @@ public class RainfallAnalyser {
 
     }
 
-    private static void generateMonthlyStatistics(String filename) {
-        String outputFile = generateOutputfile(filename);
+    private static void generateMonthlyStatistics(String outputFile) {
+
 
         //read the first line and ignore it
-        
+        String line = TextIO.getln();
 
+        //for testing only = it is not satisfied the required
+        TextIO.writeFile(outputFile);
+
+        //TextIO.writeFile(generateOutputfile(filename));
+
+        TextIO.putln("Year,Month,Total,Min,Max");
+
+        //testing
+        System.out.println(line);
+
+
+
+        if(TextIO.eof()){
+            System.out.println("Empty file. Aborted");
+            return;
+        }
+
+        //start extract the records
+
+        //testing : read second line
+
+        int numOfRecords = 0;
+        String[] record;
+
+
+        if(!TextIO.eof()){
+            line = TextIO.getln().trim();
+            numOfRecords ++;
+            //extract info...
+            record = line.split(",");
+            //...
+        }
+
+        while (!TextIO.eof()){
+
+            //and read again
+            line = TextIO.getln().trim();
+            numOfRecords ++;
+            //extract info...
+            record = line.split(",");
+            //
+
+        }
+
+        System.out.println(numOfRecords);
+
+        /*
+        System.out.println(Arrays.toString(record));
+
+        for(int i = 0; i < record.length; i++){
+            System.out.println(record[i]);
+            }
+         */
     }
 
     private static String generateOutputfile(String filename) {
 
-        String outputFile = "abc_analysed.csv";
+        String[] fileParts = filename.split("\\.");
+
+        System.out.println(Arrays.toString(fileParts));
+        String outputFile = fileParts[0] + "_analyed.csv";
 
         return outputFile;
     }
